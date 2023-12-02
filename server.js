273854f2fc091d097121a9aedcf4d2056f74023e
Mohammad Kiyam <moekiyam20@gmail.com>
2023-12-02 17:53:22 -0500

    prod stuff

--- server.js
+++ server.js
@@ -72,8 +72,8 @@ io.on('connection', (socket) => {
     });
 });
 
-// Start the server on port 3000
-const PORT = 3000;
+// listen to the port porivded by heroku
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 //io.emit('turnUpdate', currentPlayerTurn);
